b36d175dfd764315041789e798c325fa5e7b5617
PEGA5Y5 <yashraj1822@yahoo.com>
2022-09-06 19:06:13 +0530

    Initialised the project

---
+++ src/main/java/com/org/cricketleagueapp/CricketLeagueAppApplication.java
@@ -0,0 +1,13 @@
+package com.org.cricketleagueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketLeagueAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketLeagueAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/cricketleagueapp/CricketLeagueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.cricketleagueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketLeagueAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
