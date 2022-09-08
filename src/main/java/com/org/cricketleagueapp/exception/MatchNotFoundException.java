29e0065fcb95e479c2d1ceaef793176b4066e35f
yashvshinde <yashvshinde16@gmail.com>
2022-09-08 12:08:27 +0530

    MatchNotFoundException

--- src/main/java/com/org/cricketleagueapp/exception/MatchNotFoundException.java
+++ src/main/java/com/org/cricketleagueapp/exception/MatchNotFoundException.java
@@ -3,7 +3,6 @@ package com.org.cricketleagueapp.exception;
 public class MatchNotFoundException extends RuntimeException {
 
 	public MatchNotFoundException(String message) {
-		// TODO Auto-generated constructor stub
 		super(message);
 	}
 }
