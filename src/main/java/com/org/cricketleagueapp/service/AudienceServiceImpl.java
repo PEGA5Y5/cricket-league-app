c44b583e25728a50b633a92f522fe5be4b07fb9a
ankitasutar <ankitasutar2422@gmail.com>
2022-09-08 10:44:37 +0530

    Audience and ticket entities are modified; Service implemented;Created exception classes for both

---
+++ src/main/java/com/org/cricketleagueapp/service/AudienceServiceImpl.java
@@ -0,0 +1,75 @@
+package com.org.cricketleagueapp.service;
+
+import java.util.List;
+
+
+import com.org.cricketleagueapp.entity.Audience;
+import com.org.cricketleagueapp.entity.Match;
+import com.org.cricketleagueapp.entity.Ticket;
+import com.org.cricketleagueapp.exception.AudienceNotFoundException;
+import com.org.cricketleagueapp.exception.MatchNotFoundException;
+import com.org.cricketleagueapp.exception.TicketNotFoundException;
+import com.org.cricketleagueapp.repository.AudienceRepository;
+import com.org.cricketleagueapp.repository.MatchRepository;
+import com.org.cricketleagueapp.repository.TicketRepository;
+
+public class AudienceServiceImpl implements IAudienceService {
+	
+	private AudienceRepository audienceRepository;
+	private MatchRepository matchRepository;
+	private TicketRepository ticketRepository;
+	
+
+	public AudienceServiceImpl(AudienceRepository audienceRepository) {
+		super();
+		this.audienceRepository = audienceRepository;
+	}
+
+	@Override
+	public Audience getAudience(int audienceId) {
+		// TODO Auto-generated method stub
+		return audienceRepository.findById(audienceId).orElseThrow(() -> new AudienceNotFoundException("Audience not found with id: " + audienceId));
+		
+	}
+
+	@Override
+	public Audience insertAudience(Audience audience) {
+		// TODO Auto-generated method stub
+		return audienceRepository.save(audience);
+	}
+
+	@Override
+	public Match getMatch(int audienceId) {
+		// TODO Auto-generated method stub
+		
+		Audience audience=audienceRepository.findById(audienceId).orElseThrow(() -> new AudienceNotFoundException("Audience not found with id: " + audienceId));;
+		return audience.getMatches();
+	
+	}
+
+	@Override
+	public List<Match> getAllMatches() {
+		// TODO Auto-generated method stub
+		return matchRepository.findAll();
+	}
+
+	@Override
+	public List<Ticket> getAllTickets() {
+		// TODO Auto-generated method stub
+		return ticketRepository.findAll();
+		
+	}
+
+	@Override
+	public Ticket getTicket(int ticketId) {
+		// TODO Auto-generated method stub
+		return ticketRepository.findById(ticketId).orElseThrow(() -> new TicketNotFoundException("Ticket not found with ticketid: " + ticketId));
+	}
+
+	@Override
+	public double getPaidAmountForAllTickects() {
+		// TODO Auto-generated method stub
+		return audienceRepository.getPaidAmountForAllTickects();
+	}
+
+}
