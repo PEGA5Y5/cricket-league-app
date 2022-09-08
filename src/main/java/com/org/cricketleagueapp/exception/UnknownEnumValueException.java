625e2f08007c4e0176ba96c14dd7315b76ea3eda
PEGA5Y5 <yashraj1822@yahoo.com>
2022-09-08 12:09:59 +0530

    Enum Skill converter added

---
+++ src/main/java/com/org/cricketleagueapp/exception/UnknownEnumValueException.java
@@ -0,0 +1,9 @@
+package com.org.cricketleagueapp.exception;
+
+public class UnknownEnumValueException extends RuntimeException{
+
+	public UnknownEnumValueException(String message) {
+		super(message);
+	}
+
+}
